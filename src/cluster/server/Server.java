4bcdfcba2645e54cd221ae3e9c1a0438ff859ab4
ghe10 <gche1993@gmail.com>
2017-09-17 16:04:08 -0500

    Add zooKeeper server config and server main

--- src/cluster/server/Server.java
+++ src/cluster/server/Server.java
@@ -3,30 +3,27 @@ package cluster.server;
 import org.apache.zookeeper.server.quorum.QuorumPeer;
 import org.apache.zookeeper.server.quorum.QuorumPeerConfig;
 import org.apache.zookeeper.server.quorum.QuorumPeerMain;
+import org.apache.zookeeper.server.quorum.QuorumStats;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
 
 /**
  * This file defines the zooKeeper server bootstrap file
  */
 
 public class Server extends QuorumPeerMain implements Runnable {
-    private int serverId = -1;
-    private int nettyServerPort = -1;
-    private String configPath = "";
-
     private QuorumPeerConfig zkConfig;
 
-    public Server(int serverId, int nettyServerPort, String configPath) {
-        this.serverId = serverId;
-        this.nettyServerPort = nettyServerPort;
-        this.configPath = configPath;
-
+    public Server(String configPath) {
         zkConfig = new QuorumPeerConfig();
         try {
             zkConfig.parse(configPath);
         } catch (QuorumPeerConfig.ConfigException exception) {
-            System.err.println("*************** server load config failed ****************");
+            System.err.println("*************** server load config failed  ****************");
+            System.err.println(String.format("%s", exception.getCause()));
+            exception.printStackTrace();
         }
     }
 
@@ -57,7 +54,8 @@ public class Server extends QuorumPeerMain implements Runnable {
         if (quorumPeer == null) {
             return false;
         }
-        return quorumPeer.getState().equals(QuorumPeer.ServerState.LEADING);
+        System.out.println(quorumPeer.getServerState());
+        return quorumPeer.getServerState().equals(QuorumStats.Provider.LEADING_STATE);
     }
 
     public boolean shutDown() {
@@ -66,5 +64,27 @@ public class Server extends QuorumPeerMain implements Runnable {
         }
         quorumPeer.shutdown();
         return quorumPeer.isRunning();
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        File folder = new File("src/");
+        File[] listOfFiles = folder.listFiles();
+        for (int i = 0; i < listOfFiles.length; i++) {
+            if (listOfFiles[i].isFile()) {
+                System.out.println("File " + listOfFiles[i].getName());
+            } else if (listOfFiles[i].isDirectory()) {
+                System.out.println("Directory " + listOfFiles[i].getName());
+            }
+        }
+        Server server = new Server("src/cluster/config/configuration.cfg");
+        Thread t = new Thread(server);
+        t.start();
+        scanner.next();
+        System.out.println(server.isLeader());
+        System.out.println(server.isRunning());
+        server.shutDown();
+        System.out.println(server.isRunning());
+        System.exit(-1);
     }
 }
