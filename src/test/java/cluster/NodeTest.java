304d1a1c0553bfb9a20a7306bd908a095f542fa0
ghe10 <gche1993@gmail.com>
2017-11-09 12:01:03 -0600

    Add before part for node test

--- src/test/java/cluster/NodeTest.java
+++ src/test/java/cluster/NodeTest.java
@@ -1,5 +1,24 @@
 package cluster;
 
+import org.apache.zookeeper.ZooKeeper;
+import org.junit.Before;
+import utils.ObservableList;
+
+import java.net.UnknownHostException;
+
+import static org.mockito.Mockito.mock;
+
 public class NodeTest {
+    private ObservableList observableList;
+    private ZooKeeper zooKeeper;
+    private Node node;
+
+    @Before
+    public void prepare() throws UnknownHostException {
+        observableList = mock(ObservableList.class);
+        zooKeeper = mock(ZooKeeper.class);
+        node = new Node(zooKeeper, observableList);
+    }
+
 
 }
