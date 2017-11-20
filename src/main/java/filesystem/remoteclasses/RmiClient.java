57f322825d0fd6bf67ece921a15383a17372f6a7
ghe10 <gche1993@gmail.com>
2017-11-19 18:12:34 -0600

    Add basic client

---
+++ src/main/java/filesystem/remoteclasses/RmiClient.java
@@ -0,0 +1,14 @@
+package filesystem.remoteclasses;
+
+public class RmiClient {
+
+    private String getMasterIp() {
+        // TODO: getMasterIp
+        return "tmp";
+    }
+
+    public static void main(String args[]) {
+        // TODO: operation
+    }
+
+}
